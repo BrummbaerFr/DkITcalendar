1fb632b6b71061dd438baa459257b2fa4cc24579
Richard BLONDEL <richard.eric.blondel@gmail.com>
2018-09-21 19:15:01 +0100

    Classes read and .ics file generated successfully

--- calendar.js
+++ calendar.js
@@ -1,9 +1,20 @@
 document.body.style.border = "5px solid red";
 
+function download(file) {
+	console.log(file["file"]);
+
+	var blob = new Blob([file["file"]], {type : 'text/plain'});
+	var objectURL = URL.createObjectURL(blob);
+
+	browser.downloads.download({filename: file["filename"], url: objectURL});
+}
+
 browser.browserAction.onClicked.addListener((tab) => {
 	if (tab.url.includes("timetables.dkit.ie")) {
 		browser.tabs.executeScript({
 			file: "generate.js"
 		});
+
+		browser.runtime.onMessage.addListener(download);
 	}
 });
